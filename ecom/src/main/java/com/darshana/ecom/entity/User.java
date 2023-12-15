81b58bd0e94597369eeae96125cd9378e36f1a95
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-15 18:48:13 +0530

    Connect DB and Create User Class

---
+++ ecom/src/main/java/com/darshana/ecom/entity/User.java
@@ -0,0 +1,30 @@
+package com.darshana.ecom.entity;
+
+import com.darshana.ecom.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
+
+
+
+}
---
+++ ecom/src/main/java/com/darshana/ecom/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.darshana.ecom.enums;
+
+public enum UserRole {
+
+    ADMIN,
+    CUSTOMER
+}
