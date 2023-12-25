55578f9b64910e765fdbf1bbc629e8505b5c30d8
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-25 17:04:47 +0530

    Creating Get Product by ID & Update Product Details APIs.

--- ecom/src/main/java/com/darshana/ecom/controller/admin/AdminProductController.java
+++ ecom/src/main/java/com/darshana/ecom/controller/admin/AdminProductController.java
@@ -53,4 +53,24 @@ public class AdminProductController {
     public ResponseEntity<FAQDto> postFAQ(@PathVariable Long productId, @RequestBody FAQDto faqDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(faqService.postFAQ(productId, faqDto));
     }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long productId){
+        ProductDto productDto = adminProductService.getProductById(productId);
+        if (productDto != null){
+            return ResponseEntity.ok(productDto);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/product/{productId}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable Long productId, @ModelAttribute ProductDto productDto) throws IOException {
+        ProductDto updatedProduct = adminProductService.updateProduct(productId, productDto);
+        if (updatedProduct != null){
+            return ResponseEntity.ok(updatedProduct);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
